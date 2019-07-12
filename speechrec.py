5cd1a8452faeb2242f510f4b3a0597f1701a2b6d
BlakkM9 <julian.desloges@gmail.com>
2019-07-12 06:14:38 +0200

    Added keyword checking and keyword list

--- speechrec.py
+++ speechrec.py
@@ -3,11 +3,24 @@ from pydub import AudioSegment
 import os
 import time
 
+CHECK_INTERVALL = 5 # in seconds
+
 def main():
     # set ffmpeg path
     AudioSegment.converter = os.path.dirname(os.path.abspath(__file__)) + "\\ffmpeg\\bin\\ffmpeg"
 
-    # start loop that checks if new recording file is present
+    global keywords
+
+    # load keywords from file
+    fo = open("keywords.txt", "r")
+    keywords_combined = fo.read()
+    keywords = keywords_combined.split(",")
+
+    s_print("Keywords(" + str(len(keywords)) + "):")
+    for i in keywords:
+        s_print(i)
+
+    # start loop that checks if new file is ready
     while True:
         f = []
         for (dirpath, dirnames, filenames) in os.walk("./rec"):
@@ -16,7 +29,7 @@ def main():
         if ("ready.mp3" in f):
             recognize()
 
-        time.sleep(5)
+        time.sleep(CHECK_INTERVALL)
 
 def recognize():
         # convert to wav
@@ -30,14 +43,22 @@ def recognize():
         with sr.AudioFile("./rec/current.wav") as source:
             audio = r.record(source)
 
+        # delete wav file
+        os.remove("./rec/current.wav")
+
         try:
-            s_print("RESULT: " + r.recognize_google(audio, language="de-DE"))
+            result = r.recognize_google(audio, language="de-DE")
+            s_print("RESULT: " + result)
+
+            # check if result contains one of the keywords
+            for i in keywords:
+                if " " + i + " " in result:
+                    s_print("KEYWORD FOUND: " + i)
         except sr.UnknownValueError:
             s_print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             s_print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-        os.remove("./rec/current.wav")
 
 def s_print(string):
     print("[S] " + string)
--- start.py
+++ start.py
@@ -2,8 +2,18 @@ import os
 import subprocess
 
 # create rec dir if not existing
-if (not(os.path.exists("./rec") and os.path.isdir("./rec"))):
+if (not os.path.isdir("./rec")):
     os.mkdir("./rec")
+
+# remove all files in record (if present)
+if (os.path.isfile("current.wav")):
+    os.remove("current.wav")
+
+if (os.path.isfile("ready.mp3")):
+    os.remove("ready.mp3")
+
+if (os.path.isfile("recording.mp3")):
+    os.remove("recording.mp3")
 
 # start stream saver
 a = subprocess.Popen(["python", "audiostream.py"])
