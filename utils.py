83b51a54c6f5c0e55810da000a57da9b381ea2fb
BlakkM9 <julian.desloges@gmail.com>
2019-07-13 17:52:01 +0200

    Moved string combining to utils

--- utils.py
+++ utils.py
@@ -6,3 +6,13 @@ def get_radio_name(headers):
     else:
         radio_name = "Unknown Name"
     return radio_name
+
+def combine_string(string_arr, seperator):
+        combined = "";
+
+        for i in range(0, len(string_arr)):
+            combined += string_arr[i]
+            if i < (len(string_arr) - 1):
+                combined += seperator
+
+        return combined
