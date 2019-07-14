029d76acaca57f80174bb05a15bbbe8eaddd07c2
BlakkM9 <julian.desloges@gmail.com>
2019-07-14 03:47:18 +0200

    Improved rec dir clearing

--- start.py
+++ start.py
@@ -2,21 +2,12 @@ import os
 import subprocess
 
 # create rec dir if not existing
-if (not os.path.isdir("./rec")):
+if not os.path.isdir("./rec"):
     os.mkdir("./rec")
 
-# remove all files in record (if present)
-if (os.path.isfile("./rec/current.wav")):
-    os.remove("./rec/current.wav")
-
-if (os.path.isfile("./rec/ready.mp3")):
-    os.remove("./rec/ready.mp3")
-
-if (os.path.isfile("./rec/recording.mp3")):
-    os.remove("./rec/recording.mp3")
-
-if (os.path.isfile("./rec/current.mp3")):
-    os.remove("./rec/current.mp3")
+# remove all files in rec
+for item in os.listdir("./rec"):
+    os.remove("./rec/" + item)
 
 # start stream saver
 a = subprocess.Popen(["python", "audiostream.py"])
